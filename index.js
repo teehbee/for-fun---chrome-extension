c7b627ed63efcccb38e167b4372731666e38f2ad
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-08-12 11:17:46 +0200

    add localstorage saving and fetching

--- index.js
+++ index.js
@@ -3,21 +3,38 @@ const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 let inputBtn = document.querySelector("#input-btn");
 
+// Get stored leads from localStorage
+
+let leadsFromLocalStorage = localStorage.getItem("myLeads");
+
+if (leadsFromLocalStorage) {
+  // Check if leadsFromLocalStorage is not null before rendering
+  myLeads = JSON.parse(leadsFromLocalStorage);
+  renderLeads();
+}
+console.log("Test" + " " + leadsFromLocalStorage);
+
+// Eventlistener for saving leads
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+
+  // Turn array into string before saving in localStorage
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
 });
+
+// Function for rendering leads
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `
-            <li><a href=${myLeads[i]}>${myLeads[i]}</a></li>`;
-    console.log(listItems[i]);
+            <li><a target='_blank' href="${myLeads[i]}">${myLeads[i]}</a></li>`;
   }
+  // Adding leads to extension as a list item
 
   ulEl.innerHTML = listItems;
-
-  console.log(myLeads);
 }
