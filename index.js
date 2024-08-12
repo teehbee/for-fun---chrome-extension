df31df3428b5a3e6656c85aa4da2e4641237341d
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-08-12 12:31:35 +0200

    add functionality for saving tabs with google api

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
-let inputBtn = document.querySelector("#input-btn");
+const inputBtn = document.querySelector("#input-btn");
+const tabBtn = document.querySelector("#tab-btn");
+const deleteBtn = document.querySelector("#delete-btn");
 
 // Get stored leads from localStorage
 
-let leadsFromLocalStorage = localStorage.getItem("myLeads");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
-  // Check if leadsFromLocalStorage is not null before rendering
-  myLeads = JSON.parse(leadsFromLocalStorage);
-  renderLeads();
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
-console.log("Test" + " " + leadsFromLocalStorage);
+
+// Functionality for saving open tab in browser to lead tracker with google API
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+// Function for rendering leads
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+            <li><a target='_blank' href="${leads[i]}">${leads[i]}</a></li>`;
+  }
+  // Adding leads to extension as a list item
+
+  ulEl.innerHTML = listItems;
+}
+
+// Functionality for deleting leads
+
+deleteBtn.addEventListener("click", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
 // Eventlistener for saving leads
 
@@ -23,18 +54,5 @@ inputBtn.addEventListener("click", function () {
   // Turn array into string before saving in localStorage
 
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
 });
-
-// Function for rendering leads
-
-function renderLeads() {
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-            <li><a target='_blank' href="${myLeads[i]}">${myLeads[i]}</a></li>`;
-  }
-  // Adding leads to extension as a list item
-
-  ulEl.innerHTML = listItems;
-}
